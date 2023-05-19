8d606bf9182e8342a278adec531ab71127f9c12b
Kavitha316S <316kavithadhana@gmail.com>
2023-05-20 03:54:58 +0530

    print

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "parameter" {
-  name  = "test.test1"
-  type  = "String"
-  value = "Hello World"
+  name   = "test.test1"
+  value  = "Hello World"
+  type   = "SecureString"
+  key_id = "5678aa43-fa8a-405b-8571-1918b26d16bc"
 }
