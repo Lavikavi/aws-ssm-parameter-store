ff4cf36673921c2d9a022ba1f58e3ae9058d4110
Kavitha316S <316kavithadhana@gmail.com>
2023-05-20 04:22:41 +0530

    print

---
+++ env-dev/main.tfvars
@@ -0,0 +1,4 @@
+parameters = [
+  { name = "dev.frontend.catalogue_url", value = "http://catalogue-dev.devopsb62.online:8080/" },
+  { name = "dev.frontend.cart_url", value = "http://cart-dev.devopsb62.online:8080/" }
+]
--- main.tf
+++ main.tf
@@ -1,6 +1,7 @@
-resource "aws_ssm_parameter" "parameter" {
-  name   = "test.test1"
-  value  = "Hello World"
-  type   = "SecureString"
+resource "aws_ssm_parameter" "parameters" {
+  count  = length(var.parameters)
+  name   = var.parameters[count.index].name
+  value  = var.parameters[count.index].value
+  type   = "String"
   key_id = "5678aa43-fa8a-405b-8571-1918b26d16bc"
 }
