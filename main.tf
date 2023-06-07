cab253d2bc32f458a83532dd10e7b12a0dca8191
Kavitha316S <316kavithadhana@gmail.com>
2023-06-08 01:55:09 +0530

    print

--- main.tf
+++ main.tf
@@ -3,5 +3,5 @@ resource "aws_ssm_parameter" "parameters" {
   name   = var.parameters[count.index].name
   value  = var.parameters[count.index].value
   type   = "String"
-  key_id = "5678aa43-fa8a-405b-8571-1918b26d16bc"
+  key_id = "b5bf560a-8bf1-4778-b9b4-bcaf1686715a"
 }
